358069b08e053f69eabb53870bb463468b73538f
KritikKreativeLab <74385839+KritikKreativeLab@users.noreply.github.com>
2020-12-25 14:42:21 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,22 @@
+
+class Box {
+    constructor (x, y, w, h) {
+        var options = {
+            isStatic : true
+        };
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.w = w;
+        this.h = h;
+        this.x = x;
+        this.y = y;
+        World.add(world, this.body);
+    }
+
+    display() {
+        rectMode(CENTER);
+        strokeWeight(1);
+        stroke(255);
+        fill(127);
+        rect(this.x, this.y, this.w, this.h);
+    }
+}
